008fb2d036b7ed21ed6217dd082b37c88474b052
mounikachin <mounikac890@gmail.com>
2023-11-18 18:02:22 +0530

    first commit

--- settings.py
+++ settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-TEMPLATE_DIR = 'C:\\Users\\Mounika\\Desktop\\django\\mounika\\Scripts\\project6\\templates'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
